f38030f7662a2237ebbd27c7bf5a9e964212c342
raghu5910 <raghuram5910@gmail.com>
2018-10-20 23:56:56 +0530

    reactify django

---
+++ frontend/build/precache-manifest.f1df5334f0bf9e90f1e6142b36cf3a21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "544d614d11df92c0452f",
+    "url": "/static/js/main.544d614d.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "544d614d11df92c0452f",
+    "url": "/static/css/main.e84628ce.chunk.css"
+  },
+  {
+    "revision": "7bb8be440b751f2b0a76b449a641863e",
+    "url": "/index.html"
+  }
+];
